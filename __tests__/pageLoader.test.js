8dc1236270d50b2af34329a066dbbb0be5a33ae2
Dmitry Kabanov <dmitrykabanov@Mac-mini-Dmitrij.local>
2019-06-10 18:34:24 +0500

    добавляет заготовку для тестов

---
+++ __tests__/pageLoader.test.js
@@ -0,0 +1,9 @@
+import 'regenerator-runtime/runtime';
+// import { promises as fs } from 'fs';
+// import assert from 'power-assert';
+// import nock from 'nock';
+// import pageLoader from '../src';
+
+test('Correctly downloads the page at the specified address', async () => {
+
+});
