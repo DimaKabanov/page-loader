03db4d426862e919e8779bd4716c9dc981634507
Dmitry Kabanov <dmitrykabanov@Mac-mini-Dmitrij.local>
2019-06-10 10:38:07 +0500

    удаляет лишние файлы

--- __tests__/half.test.js
+++
@@ -1,8 +0,0 @@
-import assert from 'power-assert';
-import half from '../src';
-
-test('half', () => {
-  const actual = half(6);
-  const expected = 3;
-  assert.strictEqual(actual, expected);
-});
